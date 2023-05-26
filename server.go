2092897a5bec593db3f845201d27e4099feb67bb
Zero <tobewhatwewant@gmail.com>
2023-05-26 16:29:47 +0800

    fix: azure openai service chat completions stream => text/event-stream missing \n

--- server.go
+++ server.go
@@ -46,35 +46,34 @@ func Server(cfg *Config) error {
 		// 1. Chat Completions
 		{
 			path := "/chat/completions"
-			r.Post(path, zoox.WrapH(proxy.New(&proxy.Config{
-				OnRequest: func(req, originReq *http.Request) error {
-					req.URL.Scheme = "https"
-					req.URL.Host = fmt.Sprintf("%s.openai.azure.com", cfg.Models.ChatCompletions.Resource)
-					req.URL.Path = fmt.Sprintf("/openai/deployments/%s%s", cfg.Models.ChatCompletions.Deployment, path)
-					req.Host = req.URL.Host
+			r.Post(path, func(ctx *zoox.Context) {
+				zoox.WrapH(proxy.New(&proxy.Config{
+					OnRequest: func(req, originReq *http.Request) error {
+						req.URL.Scheme = "https"
+						req.URL.Host = fmt.Sprintf("%s.openai.azure.com", cfg.Models.ChatCompletions.Resource)
+						req.URL.Path = fmt.Sprintf("/openai/deployments/%s%s", cfg.Models.ChatCompletions.Deployment, path)
+						req.Host = req.URL.Host
 
-					originQuery := req.URL.Query()
-					originQuery.Set("api-version", cfg.APIVersion)
-					req.URL.RawQuery = originQuery.Encode()
+						originQuery := req.URL.Query()
+						originQuery.Set("api-version", cfg.APIVersion)
+						req.URL.RawQuery = originQuery.Encode()
 
-					req.Header.Del(headers.Authorization)
-					req.Header.Set(headers.Host, req.URL.Host)
-					req.Header.Set(headers.Origin, fmt.Sprintf("%s://%s", req.URL.Scheme, req.URL.Host))
-					req.Header.Set("api-key", cfg.APIKey)
+						req.Header.Del(headers.Authorization)
+						req.Header.Set(headers.Host, req.URL.Host)
+						req.Header.Set(headers.Origin, fmt.Sprintf("%s://%s", req.URL.Scheme, req.URL.Host))
+						req.Header.Set("api-key", cfg.APIKey)
 
-					logger.Infof("proxying request [%s] %s -> %s", path, originReq.URL, req.URL.String())
+						logger.Infof("[proxy] %s -> %s", originReq.URL, req.URL.String())
 
-					return nil
-				},
-				OnResponse: func(res *http.Response, originReq *http.Request) error {
-					// // issue: https://github.com/Chanzhaoyu/chatgpt-web/issues/831
-					// if res.Header.Get("Content-Type") == "text/event-stream" {
-					// 	res.Body = &NewBody{Body: res.Body}
-					// }
+						return nil
+					},
+				}))(ctx)
 
-					return nil
-				},
-			})))
+				// issue: https://github.com/Chanzhaoyu/chatgpt-web/issues/831
+				if ctx.Writer.Header().Get(headers.ContentType) == "text/event-stream" {
+					ctx.Write([]byte{'\n'})
+				}
+			})
 		}
 
 		// 2. Embeddings
@@ -106,26 +105,3 @@ func Server(cfg *Config) error {
 
 	return app.Run(fmt.Sprintf(":%d", cfg.Port))
 }
-
-// type NewBody struct {
-// 	Body io.ReadCloser
-// }
-
-// func (b *NewBody) Close() error {
-// 	return b.Body.Close()
-// }
-
-// func (b *NewBody) Read(p []byte) (n int, err error) {
-// 	n, err = b.Body.Read(p)
-// 	if err != nil {
-// 		return n, err
-// 	}
-
-// 	if n != len(p) {
-// 		fmt.Println("read", n, len(p))
-// 		copy(p[n:], []byte{'\n'})
-// 		return n + 1, nil
-// 	}
-
-// 	return n, nil
-// }
